71b3cd4839be1e67ac0f29680538821e2dd116b4
Aditya <spectraldoy@g.ucla.edu>
2023-11-20 19:41:25 -0800

    standardize pkt

--- utils.h
+++ utils.h
@@ -11,40 +11,38 @@
 #define CLIENT_PORT 6001
 #define SERVER_PORT 6002
 #define CLIENT_PORT_TO 5001
-#define PAYLOAD_SIZE 1024
+#define PAYLOAD_SIZE 1190
 #define WINDOW_SIZE 5
-#define TIMEOUT 2
-#define MAX_SEQUENCE 1024
-
-
+#define TIMEOUT 0.2
+#define MAX_SEQUENCE 1190
 
 // Packet Layout
 // You may change this if you want to
 struct packet {
-    unsigned short seqnum;
-    unsigned short acknum;
-    char ack;
-    char last;
-    unsigned int length;
+    packet(uint16_t seqnum, uint16_t acknum, uint8_t last, uint8_t ack, uint32_t length, const char* payload) {
+        this->seqnum = seqnum;
+        this->seqnum = seqnum;
+        this->acknum = acknum;
+        this->ack = ack;
+        this->last = last;
+        this->length = length;
+        memcpy(this->payload, payload, length);
+    }
+
+    uint16_t seqnum;
+    uint16_t acknum; 
+    uint8_t ack;
+    uint8_t last;
+    uint32_t length;
     char payload[PAYLOAD_SIZE];
 };
 
-// Utility function to build a packet
-void build_packet(struct packet* pkt, unsigned short seqnum, unsigned short acknum, char last, char ack,unsigned int length, const char* payload) {
-    pkt->seqnum = seqnum;
-    pkt->acknum = acknum;
-    pkt->ack = ack;
-    pkt->last = last;
-    pkt->length = length;
-    memcpy(pkt->payload, payload, length);
-}
-
 // Utility function to print a packet
-void printRecv(struct packet* pkt) {
+void printRecv(packet* pkt) {
     printf("RECV %d %d%s%s\n", pkt->seqnum, pkt->acknum, pkt->last ? " LAST": "", (pkt->ack) ? " ACK": "");
 }
 
-void printSend(struct packet* pkt, int resend) {
+void printSend(packet* pkt, int resend) {
     if (resend)
         printf("RESEND %d %d%s%s\n", pkt->seqnum, pkt->acknum, pkt->last ? " LAST": "", pkt->ack ? " ACK": "");
     else
